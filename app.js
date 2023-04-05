569b245edf1c2cd87381d14dee4226bef6abcab9
gonasooc <doupler@gmail.com>
2023-04-05 16:43:03 +0900

    css

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+const saveBtn = document.getElementById('save');
+const textInput = document.getElementById('text');
 const fileInput = document.getElementById('file');
 const modeBtn = document.getElementById('mode-btn');
 const destroyBtn = document.getElementById('destroy-btn');
@@ -15,6 +17,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = 'round'; // 라운 끝이 둥근 옵션
 let isPainting = false;
 let isFilling = false;
 
@@ -85,7 +88,7 @@ function onEraserClick() {
 
 function onFileChange(event) {
   const file = event.target.files[0];
-  const url = URL.createObjectURL(file); // 파일이 가리키는 url을 얻는 방식`
+  const url = URL.createObjectURL(file); // 파일이 가리키는 url을 얻는 방식
   console.log(url);
   const image = new Image(); // new Image()는 html에서 <img src=""></img> 이것과 동일
   image.src = url;
@@ -94,6 +97,28 @@ function onFileChange(event) {
   }
 }
 
+function onDoubleClick(event) {
+  const text = textInput.value.trim(); // 앞뒤 공백 제거
+  if(text.length > 0) {
+    ctx.save(); // 이전 값을 저정하고, 중간에 변경값이 있더라도,
+    ctx.lineWidth = 1; // 글자 입력 시 1로 변경
+    ctx.font = '68px serif',
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore(); // 끝에서 이전 저장값을 복구해줌
+  } else {
+    alert('텍스트를 입력하세요')
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL(); // base64로 인코딩된 이미지를 돌려줌
+  const a = document.createElement('a'); // fake link 생성한 후에 다운로드 세팅
+  a.href = url; // href에 해당 url 부여
+  a.download = "myDrawing.png"; // download 시 설정될 파일명
+  a.click();
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
@@ -108,3 +133,4 @@ modeBtn.addEventListener('click', onModeClick);
 destroyBtn.addEventListener('click', onDestroyClick);
 eraserBtn.addEventListener('click', onEraserClick);
 fileInput.addEventListener('change', onFileChange);
+saveBtn.addEventListener('click', onSaveClick);
